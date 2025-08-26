152996194e7ffd143685ae58569691689bf0ee1b
JeisonM025 <jeisonbma@gmail.com>
2025-08-25 19:09:09 -0500

    Datos personales

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
 
         System.out.println("COMANDOS GIT");
 
-
-
-
-
+        System.out.println("Jeison Malagon");
+        System.out.println("Seguridad");
+        System.out.println("25");
     }
 }
